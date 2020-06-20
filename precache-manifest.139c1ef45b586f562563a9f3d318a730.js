fccf7511556901bcdd8a9e55303c8bdb2a93bc85
aleksandrovfe <aleksandrov7296@gmail.com>
2020-06-20 07:53:56 +0300

    Updates

---
+++ precache-manifest.139c1ef45b586f562563a9f3d318a730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86845220978f4d321a741b21ac47db8d",
+    "url": "/three-points/index.html"
+  },
+  {
+    "revision": "f47f9c7d5c242eed9cec",
+    "url": "/three-points/static/css/main.fbc53cf4.chunk.css"
+  },
+  {
+    "revision": "ba653f549180d49b05ef",
+    "url": "/three-points/static/js/2.45ccdb42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/three-points/static/js/2.45ccdb42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f47f9c7d5c242eed9cec",
+    "url": "/three-points/static/js/main.14b6b519.chunk.js"
+  },
+  {
+    "revision": "0eba2c8e6c71e5c8d7aa",
+    "url": "/three-points/static/js/runtime-main.43984c76.js"
+  }
+]);
