8d6342f9a33bc72f181b80896bedd886fefd6dc4
aleksandrovfe <aleksandrov7296@gmail.com>
2020-06-20 07:53:18 +0300

    finish

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>3 Балла</h1>
+      <p>
+        Натуральные фруктовые соки, йогурт, черный шоколад, плавленный сыр, твердые сорта сыра, пшено.
+      </p> 
+    </div>
+  );
+}
+
+export default App;
